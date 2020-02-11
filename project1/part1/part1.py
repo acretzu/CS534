6b554bc78741ab8eff673a3f03b26e7c67850450
Adrian Cretzu <acretzu@wpi.edu>
2020-02-10 23:59:34 -0500

    Added input options. Created dummy file foo.txt

--- project1/part1/part1.py
+++ project1/part1/part1.py
@@ -1,3 +1,45 @@
 #! /usr/bin/python
 
+import os
+import sys
+import re
+from optparse import OptionParser
+
+__options__ = None
+
+#
+# parse command line
+#
+def parse_cmd_line_options():
+    parser = OptionParser()
+    parser.add_option("--h", action="store", type="int", dest="heuristic", default=1, help="The heuristic.")
+    parser.add_option("--a", action="store", type="int", dest="algorithm", default=1, help="The algorithm.")
+    parser.add_option("--f", action="store", type="string", dest="csv", default="", help="The local path to the CSV file.")
+
+    (options, args) = parser.parse_args()
+
+    # Check that all options have been provided
+    if not options.heuristic:
+        print("Execution requires heurisitic (1 for H1 or 2 for H2).")
+        sys.exit(1)
+
+    if not os.path.isfile(options.csv):
+        print("Execution requires path to CSV file.")
+        sys.exit(1)
+
+    if not options.algorithm:
+        print("Execution requires algorithm. (1 for A* or 2 for Hill Climbing).")
+        sys.exit(1)
+
+    return options
+
+
+
+
+
+#####################
+# Script Start
+#####################
 print('Hello World!')
+
+__options__ = parse_cmd_line_options()
