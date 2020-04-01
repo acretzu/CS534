86c5746070aeb07c93c0d497194fd1d99491bff0
tfni <tni@wpi.edu>
2020-04-01 12:52:38 -0400

    Update
    Add kmeans_initial_starting_centers
    Modify plot log-likelihood
    Ignore plot bic

--- project2/part2/part2.py
+++ project2/part2/part2.py
@@ -12,6 +12,7 @@ import copy
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn import datasets
+from sklearn.cluster import KMeans
 
 
 
@@ -89,6 +90,22 @@ def initial_starting_centers(data, k):
 
     return k_center, k_cov
 
+def kmeans_initial_starting_centers(data, k):
+
+    m = data.shape[1]
+
+    # randomly choose 70% of the data to do the kmeans
+    subset_number = int(data.shape[0]*0.7)
+    data_subset = data[random.sample([i for i in range(data.shape[0])], k=subset_number)]
+
+    kmeans = KMeans(n_clusters = k).fit(data_subset)
+
+    k_center = kmeans.cluster_centers_
+
+    k_cov = np.array([np.identity(m, dtype=np.float64) for i in range(k)])
+
+    return k_center, k_cov
+
 
 def maximization(data, cluster_prob):
 
@@ -120,7 +137,6 @@ def maximization(data, cluster_prob):
 
     k_cov = []
 
-    # update the k centers    
     k_center = np.dot(cluster_prob.T, data) / cluster_prob.sum(axis=0).reshape(1, k).T
 
     # update the cov for each cluster
@@ -128,12 +144,8 @@ def maximization(data, cluster_prob):
         ki_cov = np.dot(np.dot((data - k_center[ki]).T, np.diag(cluster_prob[:, ki])),
                         (data - k_center[ki]))
         ki_cov_normalize = ki_cov / cluster_prob[:, ki].sum()
-        ki_cov_list = ki_cov_normalize.tolist()
 
-        k_cov.append(ki_cov_list)
-
-    # back to list
-    # k_center = k_center_np.tolist()
+        k_cov.append(ki_cov_normalize)
 
     k_cov = np.array(k_cov)
 
@@ -165,39 +177,40 @@ def expectation(data, k_center, k_cov):
     # number of data
     n = data.shape[0]
 
-    data_np = np.array(data)
-
     cluster_prob = []
 
     for k in range(k_center.shape[0]):
 
         a = 1 / (((2 * np.pi) ** (size / 2)) * (np.linalg.det(k_cov[k]) ** (1 / 2)))
-        b = (-1 / 2) * ((data_np - k_center[k]).dot(np.linalg.inv(k_cov[k]))).dot((data_np - k_center[k]).T)
 
-        #log(a^b) = b*log(a)
-        log_val = b * np.log(a)
-        #cluster_prob_k = np.diagonal(a*np.exp(b))
-        cluster_prob_k = np.diagonal(log_val)
+        b = (-1 / 2) * ((data - k_center[k]).dot(np.linalg.inv(k_cov[k]))).dot((data - k_center[k]).T)
+
+        cluster_prob_k = np.diagonal(a * np.exp(b))
 
         cluster_prob.append(cluster_prob_k)
+
     cluster_prob = np.array(cluster_prob).T
 
+    cluster_prob_normal = []
 
-    # (just remember to normalize the results in the end,
-    # since the point must belong to one of the clusters
-    # so probabilities have to sum to 1)
-    # from professor's reply
+    # n means the number of data
+    for i in range(n):
+        # if the sum of all probabilities for one data is not 0
+        # Normalize
+        if cluster_prob.sum(axis=1).reshape(n, 1)[i] != 0:
+            cluster_prob_normal.append(cluster_prob[i] / cluster_prob.sum(axis=1).reshape(n, 1)[i])
+        # if the sum of all probabilities for one data == 0
+        # Then we just keep [0,0,0,0]
+        else:
+            cluster_prob_normal.append(cluster_prob[i])
 
-    cluster_prob_nn = cluster_prob
-
-    cluster_prob = cluster_prob / cluster_prob.sum(axis=1).reshape(n, 1)
-
-    return cluster_prob, cluster_prob_nn
+    # return: the first one is normalzied probability, the second one is unnormalzied probability
+    return np.array(cluster_prob_normal), cluster_prob
 
 
 def get_loglikelihood(cluster_prob):
-    
-    total_likelihood = np.log(cluster_prob).sum()
+
+    total_likelihood = np.log(cluster_prob.sum(axis = 1)).sum()
 
     return total_likelihood
 
@@ -206,10 +219,12 @@ def get_bic(total_likelihood, n_data, m_fea, k):
 
     parameters = k*(m_fea + m_fea ** 2)
 
-    # bic = -2*total_likelihood + (math.log(n_data))*parameters
-    bic = -2*total_likelihood
+    bic = -2*total_likelihood + ((math.log(n_data))*parameters)
+    # bic = -total_likelihood + ((math.log(n_data))*parameters)
+    # bic = -2*total_likelihood + (n_data*parameters)
+    bic_wrong = -total_likelihood + ((math.log2(n_data))*parameters)
 
-    return bic
+    return bic, bic_wrong
 
 
 def plot_loglikelihood(total_likelihood_list, plot_filename = "plot_ll.png"):
@@ -217,7 +232,7 @@ def plot_loglikelihood(total_likelihood_list, plot_filename = "plot_ll.png"):
     fig, ax = plt.subplots(nrows=1, ncols=1)  # create figure & 1 axis
     fig.set_size_inches(8, 6)
 
-    ax.plot([i for i in range(len(total_likelihood_list))], total_likelihood_list)
+    ax.plot([i+1 for i in range(len(total_likelihood_list[1:]))], total_likelihood_list[1:])
 
     plt.title('log-likelihood vs. # of iterations')
     plt.xlabel('# of iterations')
@@ -230,7 +245,8 @@ def plot_loglikelihood(total_likelihood_list, plot_filename = "plot_ll.png"):
 # Main function
 def train_em(data, k, n_epochs):
 
-    k_center, k_cov = initial_starting_centers(data, k)
+    # k_center, k_cov = initial_starting_centers(data, k)
+    k_center, k_cov = kmeans_initial_starting_centers(data, k)
 
     total_likelihood_list = []
 
@@ -248,7 +264,7 @@ def train_em(data, k, n_epochs):
         k_center, k_cov = maximization(data, cluster_prob)
 
         # record likelihood
-        total_likelihood = get_loglikelihood(cluster_prob)
+        total_likelihood = get_loglikelihood(cluster_prob_nn)
         total_likelihood_list.append(total_likelihood)
 
     # save log-likelihood vs. # of iteration
@@ -275,15 +291,18 @@ def plot_bic(bic_list, k_list, plot_filename = "plot_bic.png"):
 def determine_lowest_k_using_bic(data, k_range = 10):
 
     bic_list = []
+    bic_wrong_list= []
     for ki in range(k_range):
         total_likelihood_list = train_em(data, ki + 2, 20)
 
-        bic = get_bic(total_likelihood_list[-1], data.shape[0], data.shape[1], ki+2)
+        bic, bic_wrong = get_bic(total_likelihood_list[-1], data.shape[0], data.shape[1], ki+2)
         bic_list.append(bic)
+        bic_wrong_list.append(bic_wrong)
 
     k_list = [ki + 2 for ki in range(k_range)]
 
     plot_bic(bic_list, k_list)
+    plot_bic(bic_wrong_list, k_list, "plot_bic_wrong.png")
 
 
 
@@ -296,6 +315,7 @@ def determine_lowest_k_using_bic(data, k_range = 10):
 
 # Default values
 file_name = "sample_em_data.csv"
+# file_name = "mini_sample.csv"
 num_clusters = 5
 
 
@@ -309,19 +329,19 @@ if len(sys.argv) >= 3:
     num_clusters = int(sys.argv[2])
 
 data = parse_csv_file(file_name)
-print(data)
+# print(data)
 
 
-if num_clusters == 0:
-    determine_lowest_k_using_bic(data, k_range = 20)
-else:
-    train_em(data, num_clusters, 20)
+# if num_clusters == 0:
+#     determine_lowest_k_using_bic(data, k_range = 20)
+# else:
+#     train_em(data, num_clusters, 20)
 
 
-    # iris = datasets.load_iris()
-# X = iris.data
+iris = datasets.load_iris()
+X = iris.data
 
-#determine_lowest_k_using_bic(data, k_range=20)
+determine_lowest_k_using_bic(data, k_range=20)
 # determine_lowest_k_using_bic(X, k_range=10)
 
 # total_likelihood_list, cluster_prob = train_em(X, 3, 10)
