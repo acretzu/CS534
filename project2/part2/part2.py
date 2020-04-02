3d3102dd8a9306a6cee7ae2246424b7f8b245ebe
tfni <tni@wpi.edu>
2020-04-01 23:30:26 -0400

    Final Check

--- project2/part2/part2.py
+++ project2/part2/part2.py
@@ -15,14 +15,12 @@ from sklearn import datasets
 from sklearn.cluster import KMeans
 
 
-
 #
 # Open the CSV file and get data
 # Format: <x float>, <y float>
 #
 def parse_csv_file(path_to_file):
     file_ptr = open(path_to_file, "r")
-
 
     # Error out if we can't open the file
     if not file_ptr:
@@ -52,6 +50,7 @@ def parse_csv_file(path_to_file):
 
     file_ptr.close()
     return np.array(ret_array)
+
 
 ##############################################################################
 
@@ -92,14 +91,13 @@ def initial_starting_centers(data, k):
 
 
 def kmeans_initial_starting_centers(data, k):
-
     m = data.shape[1]
 
     # randomly choose 70% of the data to do the kmeans
-    subset_number = int(data.shape[0]*0.7)
+    subset_number = int(data.shape[0] * 0.5)
     data_subset = data[random.sample([i for i in range(data.shape[0])], k=subset_number)]
 
-    kmeans = KMeans(n_clusters = k).fit(data_subset)
+    kmeans = KMeans(n_clusters=k).fit(data_subset)
 
     k_center = kmeans.cluster_centers_
 
@@ -109,7 +107,6 @@ def kmeans_initial_starting_centers(data, k):
 
 
 def maximization(data, cluster_prob):
-
     """
     adjust (mean, standard deviation) to fit the points assigned to them
 
@@ -181,7 +178,6 @@ def expectation(data, k_center, k_cov):
     cluster_prob = []
 
     for k in range(k_center.shape[0]):
-
         a = 1 / (((2 * np.pi) ** (size / 2)) * (np.linalg.det(k_cov[k]) ** (1 / 2)))
 
         b = (-1 / 2) * ((data - k_center[k]).dot(np.linalg.inv(k_cov[k]))).dot((data - k_center[k]).T)
@@ -210,27 +206,29 @@ def expectation(data, k_center, k_cov):
 
 
 def get_loglikelihood(cluster_prob):
-
-    total_likelihood = np.log(cluster_prob.sum(axis = 1)).sum()
+    total_likelihood = np.log(cluster_prob.sum(axis=1)).sum()
 
     return total_likelihood
 
 
 def get_bic(total_likelihood, n_data, m_fea, k):
+    parameters = k * (m_fea + m_fea ** 2)
 
-    parameters = k*(m_fea + m_fea ** 2)
+    # bic = -2*total_likelihood + ((math.log(n_data))*parameters)
+    # bic = -2*total_likelihood + ((math.log(n_data*(n_data +2)/24))*parameters)
+    # bic = -2*total_likelihood + 2*((math.log(n_data))*parameters)
+    # bic = -2*total_likelihood
 
-    bic = -2*total_likelihood + ((math.log(n_data))*parameters)
+    bic = -2 * total_likelihood + 3 * ((math.log2(n_data)) * parameters)
 
     return bic
 
 
-def plot_loglikelihood(total_likelihood_list, plot_filename = "plot_ll.png"):
-
+def plot_loglikelihood(total_likelihood_list, plot_filename="plot_ll.png"):
     fig, ax = plt.subplots(nrows=1, ncols=1)  # create figure & 1 axis
     fig.set_size_inches(8, 6)
 
-    ax.plot([i+1 for i in range(len(total_likelihood_list[1:]))], total_likelihood_list[1:])
+    ax.plot([i + 1 for i in range(len(total_likelihood_list[1:]))], total_likelihood_list[1:])
 
     plt.title('log-likelihood vs. # of iterations')
     plt.xlabel('# of iterations')
@@ -241,8 +239,7 @@ def plot_loglikelihood(total_likelihood_list, plot_filename = "plot_ll.png"):
 
 
 # Main function
-def train_em(data, k, time_up = 9.5, diff_thred = 0.1):
-
+def train_em(data, k, time_up=9.5, diff_thred=0.1):
     total_likelihood_series_list = []
     k_center_list = []
     k_cov_list = []
@@ -254,6 +251,8 @@ def train_em(data, k, time_up = 9.5, diff_thred = 0.1):
     while time_limit < time_up:
 
         k_center, k_cov = kmeans_initial_starting_centers(data, k)
+        # k_center, k_cov = initial_starting_centers(data, k)
+
         total_likelihood_list = []
 
         total_likelihood_old = float('-inf')
@@ -261,7 +260,6 @@ def train_em(data, k, time_up = 9.5, diff_thred = 0.1):
 
         # diff threshold and time limit
         while (diff_ll > diff_thred) & (time_limit < time_up):
-
             # expectation
             cluster_prob, cluster_prob_nn = expectation(data, k_center, k_cov)
 
@@ -298,8 +296,7 @@ def train_em(data, k, time_up = 9.5, diff_thred = 0.1):
     return best_ll_series, best_k_center, best_k_cov
 
 
-def plot_bic(bic_list, k_list, plot_filename = "plot_bic.png"):
-
+def plot_bic(bic_list, k_list, plot_filename="plot_bic.png"):
     fig, ax = plt.subplots(nrows=1, ncols=1)  # create figure & 1 axis
     fig.set_size_inches(8, 6)
 
@@ -313,26 +310,25 @@ def plot_bic(bic_list, k_list, plot_filename = "plot_bic.png"):
     plt.close(fig)
 
 
-def determine_lowest_k_using_bic(data, time_up_bic = 9.5, bic_diff_thred = 0.1):
-
+def determine_lowest_k_using_bic(data, time_up_bic=9.5, bic_diff_thred=0.01):
     start_time_bic = time.time()
 
     k_start = 3
-    bic_old = get_bic(train_em(data, k=(k_start-1), time_up= 5, diff_thred = 1)[0][-1],
+    bic_old = get_bic(train_em(data, k=(k_start - 1), time_up=2, diff_thred=1)[0][-1],
                       data.shape[0],
                       data.shape[1],
-                      k=(k_start-1))
+                      k=(k_start - 1))
 
     bic_list = []
     bic_list.append(bic_old)
     bic_diff_pct = 100
 
-    k_range = 10
+    # k_range = 16
 
     while (bic_diff_pct > bic_diff_thred) & ((time.time() - start_time_bic) < time_up_bic):
         # while k_start < k_range:
 
-        total_likelihood_list, centers, cov = train_em(data, k=k_start, time_up= 1, diff_thred = 1)
+        total_likelihood_list, centers, cov = train_em(data, k=k_start, time_up=2, diff_thred=1)
 
         bic = get_bic(total_likelihood_list[-1], data.shape[0], data.shape[1], k_start)
 
@@ -347,12 +343,12 @@ def determine_lowest_k_using_bic(data, time_up_bic = 9.5, bic_diff_thred = 0.1):
     k_list = [ki + 2 for ki in range(len(bic_list))]
     plot_bic(bic_list, k_list)
 
-    return (k_start-1), total_likelihood_list, centers, cov
+    return (k_start - 1), total_likelihood_list, centers, cov, bic
 
 
 def restart(k):
     if k == 0:
-        determine_lowest_k_using_bic(data, k_range = 13)
+        determine_lowest_k_using_bic(data, k_range=13)
     else:
         train_em(data, num_clusters, 20)
 
@@ -366,8 +362,8 @@ def restart(k):
 # Default values
 file_name = "sample_em_data.csv"
 # file_name = "mini_sample.csv"
-num_clusters = 0
-
+# file_name = "data_3_cluster.csv"
+num_clusters = 5
 
 # File is arg1
 if len(sys.argv) >= 2:
@@ -379,13 +375,13 @@ if len(sys.argv) >= 3:
     num_clusters = int(sys.argv[2])
 
 data = parse_csv_file(file_name)
-#print(data)
+# print(data)
 
 total_start_time = time.time()
 if num_clusters == 0:
-    best_k, ll_data, final_centers, final_cov = determine_lowest_k_using_bic(data,
-                                                                             bic_diff_thred = 10e-2,
-                                                                             time_up_bic=10)
+    best_k, ll_data, final_centers, final_cov, bic = determine_lowest_k_using_bic(data,
+                                                                                  bic_diff_thred=10e-4,
+                                                                                  time_up_bic=9.5)
     print("Best number of clusters:", best_k)
     print("Final Cluster Centers:")
     for k in range(len(final_centers)):
@@ -394,19 +390,21 @@ if num_clusters == 0:
         print("Cov: ", )
         print(final_cov[k])
     print("------------------------------------------------")
+    print("BIC:", bic)
+    print("LL:", ll_data[-1])
     print("Total Execution Time = ", time.time() - total_start_time)
 
 else:
-    ll_data, final_centers, final_cov = train_em(data, num_clusters, time_up = 9.5, diff_thred = 0.1)
+    ll_data, final_centers, final_cov = train_em(data, num_clusters, time_up=9.5, diff_thred=1)
     print("Final Cluster Centers:")
     for k in range(len(final_centers)):
-        print("Cluster", k+1, )
+        print("Cluster", k + 1, )
         print("Mean:", final_centers[k])
         print("Cov: ", )
         print(final_cov[k])
     print("------------------------------------------------")
+    print("LL:", ll_data[-1])
     print("Total Execution Time = ", time.time() - total_start_time)
-
 
 # iris = datasets.load_iris()
 # X = iris.data
