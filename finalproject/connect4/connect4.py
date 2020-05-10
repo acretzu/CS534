0b5ff21b86243d11c2aa4d8ab382b6341f29c497
Marc <mdreardon@wpi.edu>
2020-05-10 14:55:59 -0400

    MonteCarlo exponential loss deepening

--- finalproject/connect4/connect4.py
+++ finalproject/connect4/connect4.py
@@ -151,7 +151,7 @@ class Connect4:
         Place a piece at the given column
         """
         # Place a piece
-        print("Player", self.turn, "placed piece at column:", column + 1)
+        #print("Player", self.turn, "placed piece at column:", column + 1)
 
         # Sanity check that column is not full
         if self.board[0][column] == 1 or self.board[0][column] == -1:
@@ -235,9 +235,6 @@ class Connect4:
 
             while self.has_winner() == 0:
 
-                p1 = QLearner(1)
-                p2 = MonteCarlo(2, self, depth=5, rollouts=500)
-
                 if self.full():
                     print("It's a draw!")
                     break
@@ -255,7 +252,7 @@ class Connect4:
                         self.place(p1.random_action())
 
                     elif self.player1 == "MonteCarlo":
-                        p1 = MonteCarlo(1, self)
+                        p2 = MonteCarlo(1, self)
 
                     elif self.player1 == "NN":
                         # update
@@ -313,7 +310,9 @@ class Connect4:
 
         while self.has_winner() == 0:
 
-            opp = MonteCarlo(-1, self, depth=5, rollouts=1000)
+            print("1 2 3 4 5 6 7")
+
+            opp = MonteCarlo(-1, self, depth=50, rollouts=1000)
 
             if self.full():
                 print("It's a draw!")
@@ -321,7 +320,7 @@ class Connect4:
 
             if self.turn == player:
                 human_move = int(raw_input(">>> "))
-                self.place_with_print(human_move)
+                self.place_with_print(human_move-1)
             else:
                 opp_move = opp.choose_col()
                 while self.can_place(opp_move) is False:
@@ -329,6 +328,8 @@ class Connect4:
                     opp_move = opp.choose_col()
                 opp.print(opp.root)
                 self.place_with_print(opp_move)
+
+
 
         print("The winner is player ", self.has_winner())
         self.clear_board()
@@ -366,6 +367,8 @@ class Connect4:
 
 """ 5) Random VS MonteCarlo """
 connect4 = Connect4("Random", "MonteCarlo")
+#connect4.play(games=100)
+print(connect4.__str__())
 connect4.play_human(1)
 
 
@@ -397,9 +400,6 @@ connect4.play_human(1)
 """ MonteCarlo VS MonteCarlo """
 
 
-############
-# NOTE: has_winner() is not working correctly
-############
 
 
 
--- finalproject/connect4/monte_carlo.py
+++ finalproject/connect4/monte_carlo.py
@@ -1,4 +1,5 @@
-from copy import deepcopy
+from copy import deepcopy, copy
+from math import ceil
 from random import randrange
 
 #from connect4 import Connect4
@@ -15,7 +16,7 @@ class Node:
 
 class MonteCarlo:
 
-    def __init__(self, player, c4, depth=1, rollouts=500):
+    def __init__(self, player, c4, depth=50, rollouts=1000):
         self.root = Node(None, [])
         self.c4 = c4
         self.player = player
@@ -32,34 +33,43 @@ class MonteCarlo:
 
     def select(self):
         root = self.root
+        i = 0
         while len(root.children) is not 0:
 
             # Find Best Child
             best = Node(root, [])
             for child in root.children:
 
-                if child.count is 0:
-                    child_ratio = 0
-                else:
-                    child_ratio = child.wins / child.count
+                child_ratio = self.win_ratio(child)
+                best_ratio = self.win_ratio(best)
 
-                if best.count is 0:
+                # choose a different one
+                if best_ratio == 1:
                     best_ratio = 0
-                else:
-                    best_ratio = best.wins / best.count
+                if child_ratio == 1:
+                    child_ratio = 0
 
                 if child_ratio >= best_ratio:
                     best = child
 
+            i += 1
+
             root = best
 
+        #print(root.children)
+
+        #print("selected (", i , "): [", root.wins, root.count, "]")
+        #print(root)
         return root
 
     def expand(self, root):
+        moves = []
         for i in range(7):
             # Get prior moves and add the latest
-            moves = root.moves
+            moves = copy(root.moves)
             moves.append(i)
+            #print("root moves",root.moves)
+            #print("child" , moves)
 
             # Expand
             root.children.append(Node(root, moves))
@@ -67,25 +77,23 @@ class MonteCarlo:
     def simulate(self, game, root, col):
         # Returns 0 if loss, 1 if won, -1 if the game cant be continued through that col
 
-        if game.full() is True:
-            return -2  # dont try again
-
         if game.can_place(col) is False:
+            #print("cant place")
             return -1  # must try again at different column
 
         column = col
         while game.has_winner() is 0 and not game.full():
 
             # Place a piece
-            game.place(column)
+            if game.can_place(column) is True:
+                game.place(column)
 
             column = randrange(7)
 
-        print(game.__str__())
-        print(game.has_winner())
-        #print(self.player)
+        #print(game.has_winner())
+        #print(game.__str__())
         if game.has_winner() is self.player:
-
+            #print("win")
             root.children[col].count += 1
             root.children[col].wins += 1
             return 1
@@ -93,17 +101,45 @@ class MonteCarlo:
             root.children[col].count += 1
             return 0
 
-    def update(self, root, col):
+    def update(self, leaf):
 
-        # Get chosen child
-        leaf = root.children[col]
+        # Before update, save the past wins
+        past_wins = leaf.wins
+        past_count = leaf.count
+
+        #print("leaf:", leaf.wins, leaf.count)
+
+        # Update leaf
+        #print("children")
+        for child in leaf.children:
+            # Wins update
+            leaf.wins += child.wins
+            #print(child.wins, child.count)
+            leaf.count += child.count
+
+        #print("leaf (past wins): ", past_wins)
+        #print("leaf (wins): ", leaf.wins)
+        new_wins = leaf.wins - past_wins
+        new_count = leaf.count - past_count
+
+        #print("leaf (after):", leaf.wins, leaf.count)
 
         while leaf.parent is not None:
-            for child in leaf.parent.children:
+            #print("parent (before):", leaf.parent.wins, leaf.parent.count)
 
-                leaf.parent.wins += child.wins
-                leaf.parent.count += child.count
-                #print(leaf.parent.wins, leaf.parent.count, child.wins, child.count)
+            # Before update save past
+            #parent_past_wins = leaf.parent.wins
+
+            # Add leaf wins
+            leaf.parent.wins += new_wins
+
+            #print("new wins:", new_wins)
+            # Add rollouts
+            leaf.parent.count += new_count
+
+            #print("parent (after):",leaf.parent.wins, leaf.parent.count)
+
+            # climb up path
             leaf = leaf.parent
 
     def get_board(self, leaf):
@@ -111,10 +147,13 @@ class MonteCarlo:
 
         # Create temp board
         game = deepcopy(self.c4)
+        #print("root board:\n", game.__str__())
 
         for col in leaf.moves:
             if game.can_place(col):
                 game.place(col)
+
+        #print("selected:\n", game.__str__())
 
         return game
 
@@ -128,23 +167,26 @@ class MonteCarlo:
 
             # Get board for this child
             game = self.get_board(root)
-            #print(game.board)
+            #print("selected:\n",game.__str__())
 
             # Expand the child node
-            self.expand(root)
+            if game.has_winner() is 0 and game.full() is False:
+                self.expand(root)
 
-            # Simulate games for random children
-            for i in range(self.rollouts):
 
-                # Board is back to state of interest
-                #print(game.board)
+                #print(ceil(self.rollouts/((len(root.moves)+1)**2)))
+                # Simulate games for random children
+                for j in range(ceil(self.rollouts/((len(root.moves)+1)**2))):
 
-                col = randrange(7)
-                while self.simulate(deepcopy(game), root, col) is -1:
+                    # Board is back to state of interest
+                    #print(game.board)
+
                     col = randrange(7)
+                    while self.simulate(deepcopy(game), root, col) is -1:
+                        col = randrange(7)
 
-            # Update along chosen path
-            self.update(root, col)
+                # Update along chosen path
+                self.update(root)
 
         # Choose the best col
         best = Node(None, [])
@@ -175,7 +217,7 @@ class MonteCarlo:
         if len(root.children) is not 0:
             i = 0
             for child in root.children:
-                print(i, ": [", self.win_ratio(child), child.wins, child.count, "]")
+                #print(i+1, ": [", self.win_ratio(child), child.wins, child.count, "]")
                 i += 1
 
 
